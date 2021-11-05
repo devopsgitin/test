c37a81d402e6c9ef575ba213fe22ccdc3a476f5f
test <test@devops.com>
2021-11-05 06:03:59 +0000

    Feature 2 to subtract 2 numbers

--- test.sh
+++ test.sh
@@ -3,5 +3,8 @@ echo "Hello World!"
 echo "Feature 1 to add 2 numbers"
 a=2
 b=3
-result=$((a+b))
-echo "$a + $b = $result"
+add=$((a+b))
+echo "$a + $b = $add"
+echo "Feature 2 to substract 2 numbers"
+sub=$((a-b))
+echo "$a - $b = $sub"
